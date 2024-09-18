d0226123ed505096514160543a0db8c6dc0281c1
Aditi Maurya <maurya.ad@northeastern.edu>
2024-09-18 09:11:26 -0400

    Initial commit

---
+++ ass_1/src/model/Persondetails.java
@@ -0,0 +1,205 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author aditi
+ */
+public class Persondetails {
+    String firstname;
+    String lastname;
+    String email;
+    String phone;
+    String drlno;
+    String telno;
+    String drlst;
+    String faxno;
+    String ssn;
+    String hobbies;
+    String aadr1;
+    String lang;
+    String addr2;
+    String ed;
+    String city;
+    String visatype;
+    String state;
+    String dob;
+    String zip;
+    String gender;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDrlno() {
+        return drlno;
+    }
+
+    public void setDrlno(String drlno) {
+        this.drlno = drlno;
+    }
+
+    public String getTelno() {
+        return telno;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+
+    public String getDrlst() {
+        return drlst;
+    }
+
+    public void setDrlst(String drlst) {
+        this.drlst = drlst;
+    }
+
+    public String getFaxno() {
+        return faxno;
+    }
+
+    public void setFaxno(String faxno) {
+        this.faxno = faxno;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getAadr1() {
+        return aadr1;
+    }
+
+    public void setAadr1(String aadr1) {
+        this.aadr1 = aadr1;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getAddr2() {
+        return addr2;
+    }
+
+    public void setAddr2(String addr2) {
+        this.addr2 = addr2;
+    }
+
+    public String getEd() {
+        return ed;
+    }
+
+    public void setEd(String ed) {
+        this.ed = ed;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getVisatype() {
+        return visatype;
+    }
+
+    public void setVisatype(String visatype) {
+        this.visatype = visatype;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    /**
+     *
+     * @param dob
+     */
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+    
+  
+          
+}
